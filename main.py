a754594da7f25a45e0934d86ee3c7612364b768a
jordan10j <jordan.morales@istpet.edu.ec>
2023-12-02 18:10:37 +0000

    archivo que contiene la libreria flask

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-import cv2
+from flask import flask
+from app import views
+
+app= Flask(__name__)
+
+app.add_url_rule(rule='/', endpoint='home', view_func=views)
+app.add_url_rule(rule='app/',endpoint='app',view_func=views)
+
+if __name__ == "__main__":
+    app.run(debug=True)
